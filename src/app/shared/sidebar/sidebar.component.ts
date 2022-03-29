b42ba42aa25a6205f7a8b2eb91382c5af54363d3
Meenakshi <Meenakshi.Ravichandran@lamresearch.com>
2022-03-29 22:38:47 +0530

    changed sidebar code

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import { NavigationStart, Router } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
   showHead = false;
@@ -13,7 +13,7 @@ export class SidebarComponent implements OnInit {
     // on route change to '/login', set the variable showHead to false
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
-        
+
         if (event.url === '/audit') {
           this.showHead = true;
         } else {
